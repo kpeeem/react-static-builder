63a862997485571c3dcc95752866ba36cba18f18
Andrew Zhe <kpeeem@gmail.com>
2017-03-06 14:33:03 +0300

    add email

--- src/pages/Where/index.js
+++ src/pages/Where/index.js
@@ -11,6 +11,7 @@ export default class extends Component {
             <div className={style.main}>
                 <h2 className={style.slogan}>
                     <a className={style.link} href="http://github.com/kpeeem">GitHub</a>
+                    <a className={style.link} href="mailto:kpeeem@gmail.com">kpeeem@gmail.com</a>
                 </h2>
             </div>
         )
